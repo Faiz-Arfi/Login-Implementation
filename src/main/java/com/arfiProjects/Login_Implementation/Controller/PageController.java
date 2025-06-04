950def5be276673f93dbced325477b93e6aa23a4
Faiz Arfi <faizarfi12@gmail.com>
2025-06-04 18:28:46 +0530

    Designed home page, login page and register page

---
+++ src/main/java/com/arfiProjects/Login_Implementation/Controller/PageController.java
@@ -0,0 +1,28 @@
+package com.arfiProjects.Login_Implementation.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    // @GetMapping("/logout")
+    // public String logout() {
+    //     return "logout";
+    // }
+}
